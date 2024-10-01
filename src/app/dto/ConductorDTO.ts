1004f4e64c078bdcebd62f4ec74779640e362d17
saraagutierrez <saralgutierrez@javeriana.edu.co>
2024-10-01 17:55:54 -0500

    Inicio Entrega 2

---
+++ src/app/dto/ConductorDTO.ts
@@ -0,0 +1,9 @@
+export class ConductorDTO {
+    constructor(
+        public id: null | number,
+        public nombre: string,
+        public apellido: string,
+        public telefono: number,
+        public direccion: string
+    ){}
+}
